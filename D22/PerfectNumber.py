3e088af646c0858163e7f2ed7a99d21a9a6b9cf8
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-01-15 17:35:50 +0300

    fixed time limit exceeded on leetcode for Perfect Number on Day 22

--- D22/PerfectNumber.py
+++ D22/PerfectNumber.py
@@ -3,13 +3,11 @@ import math
 
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        sumOfDivisors = 0
-        # endpoint = num // 2 + 1
-        # print("end point ", num // 2 + 1)
-        for divisor in range(1, num // 2 + 1):
+        sumOfDivisors = 1
+        for divisor in range(2, int(num ** 0.5) + 1):
             moduloVal = num % divisor
             if not (moduloVal): # num is divisible
-                sumOfDivisors += divisor
+                sumOfDivisors += divisor + int( num / divisor )
             
         # print("sum of divisors : ", sumOfDivisors)
         # print("num is ", num)
@@ -21,4 +19,6 @@ class Solution:
                 
 s = Solution()
 answer = s.checkPerfectNumber(28)
+# answer = s.checkPerfectNumber(6)
+# answer = s.checkPerfectNumber(8128)
 print(answer)
