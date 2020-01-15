a0cc9f12c84866a1a7bcd98b439404f46a3b2344
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-01-15 18:28:31 +0300

    corrected CountPrime problem of Day 22 for given n = 2

--- D22/CountPrimes.py
+++ D22/CountPrimes.py
@@ -4,9 +4,10 @@ class Solution:
         checkPrime = [True] * (n + 1)
         # checkPrime[0] = checkPrime[1] = False
 
-        if n < 2:
+        if n <= 2:
             return 0
-        for i in range(2, n + 1):
+        
+        for i in range(2, n):
             currentI = i
             currentCheckPrime = checkPrime[i]
             if checkPrime[i]:
@@ -16,9 +17,10 @@ class Solution:
                 for j in range(i*i, n + 1, i):
                     checkPrime[j] = False
                     # j = j + i
-        return primesList
+        # return primesList
+        return len(primesList)
 
-s = Solution()
+# s = Solution()
 # result = s.countPrimes(10)
-result = s.countPrimes(20)
-print(result)
+# result = s.countPrimes(20)
+# print(result)
