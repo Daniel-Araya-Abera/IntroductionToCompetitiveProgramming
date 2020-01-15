4e94de39fba91bf0365911ee888a325bdd080d2e
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-01-15 18:26:33 +0300

    solution to problem: Count Primes Day 22, solved for most cases

---
+++ D22/CountPrimes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        primesList = []
+        checkPrime = [True] * (n + 1)
+        # checkPrime[0] = checkPrime[1] = False
+
+        if n < 2:
+            return 0
+        for i in range(2, n + 1):
+            currentI = i
+            currentCheckPrime = checkPrime[i]
+            if checkPrime[i]:
+                primesList.append(i)
+                # j = i * i
+                # while j < n + 1 :
+                for j in range(i*i, n + 1, i):
+                    checkPrime[j] = False
+                    # j = j + i
+        return primesList
+
+s = Solution()
+# result = s.countPrimes(10)
+result = s.countPrimes(20)
+print(result)
