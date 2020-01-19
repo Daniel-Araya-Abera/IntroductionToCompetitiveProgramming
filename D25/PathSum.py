135f97a78d45577c07758e49011e4ec0203453c1
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-01-19 23:15:18 +0300

    Day 25 PathSum problem solution sumbitted code

--- D25/PathSum.py
+++ D25/PathSum.py
@@ -9,7 +9,7 @@ class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         count = 1
         if not root: ## root is empty
-            print("here")
+            # print("here")
             return False
         return self.hasPathSumHelper(root, sum, 0)
         
@@ -19,48 +19,54 @@ class Solution:
             return boolVal
         
         sumUpToNow += root.val
-        if sumUpToNow == sum:
+        if sumUpToNow == sum and not root.left and not root.right:
             boolVal = True
             return boolVal
         if root.left and root.right:
             leftSide = self.hasPathSumHelper(root.left, sum, sumUpToNow)
             rightSide = self.hasPathSumHelper(root.right, sum, sumUpToNow)
+            # print("AAA")
             return leftSide or rightSide
         if root.left and not root.right:
             leftSide = self.hasPathSumHelper(root.left, sum, sumUpToNow)
+            # print("BB")
             return leftSide
         if root.right and not root.left:
             rightSide = self.hasPathSumHelper(root.right, sum, sumUpToNow)
+            # print("CC")
             return rightSide
-        else:
-            print("HERE WHY HTO")
-            print("root", root)
-            print("root.left-- > ", root.left)
-            print("root.right -- > " , root.right)
+        # else:
+        #     print("HERE WHY HTO")
+        #     print("root", root)
+        #     print("root.left-- > ", root.left)
+        #     print("root.right -- > " , root.right)
 
+
+
+##############################################################
 # s = Solution()
 # givenRoot = TreeNode(1)
-# givenRoot.left = TreeNode(2)
-# s.hasPathSum(givenRoot, 1)
+# leftSide1 = givenRoot.left = TreeNode(2)
+# leftSide1.left = leftSide1.right = None
+# result = s.hasPathSum(givenRoot, 1)
+# print("RESULT ", result)
+##############################################################
+# s = Solution()
+# givenRoot = TreeNode(5)
+# leftSide1 = givenRoot.left = TreeNode(4)
+# rightSide1 = givenRoot.right = TreeNode(8)
 
-s = Solution()
-givenRoot = TreeNode(5)
-leftSide1 = givenRoot.left = TreeNode(4)
-rightSide1 = givenRoot.right = TreeNode(8)
+# leftSide2 = leftSide1.left = TreeNode(11)
+# # leftSide2_2 = leftSide1.right = None
+# leftSide3 = leftSide2.left = TreeNode(7)
+# leftSide3_2 = leftSide2.right = TreeNode(2)
 
-leftSide2 = leftSide1.left = TreeNode(11)
-# leftSide2_2 = leftSide1.right = None
+# rightSide2 = rightSide1.left = TreeNode(13)
+# rightSide2 = rightSide1.right = TreeNode(4)
 
-leftSide3 = leftSide2.left = TreeNode(7)
-leftSide3_2 = leftSide2.right = TreeNode(2)
+# rightSide2_2 = rightSide2.right = TreeNode(1)
 
-rightSide2 = rightSide1.left = TreeNode(13)
-rightSide2 = rightSide1.right = TreeNode(4)
-
-rightSide2_2 = rightSide2.right = TreeNode(1)
-
-# [5,4,8,11,null,13,4,7,2,null,null,null,1]
-result = s.hasPathSum(givenRoot, 22)
-
-print("RESULT IS ", result)
-# s.hasPathSum(givenRoot, 1)
+# # [5,4,8,11,null,13,4,7,2,null,null,null,1]
+# result = s.hasPathSum(givenRoot, 22)
+# print("RESULT IS ", result)
+##############################################################
