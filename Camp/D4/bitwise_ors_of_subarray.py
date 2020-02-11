ae42212490933472cf30268eb29ad97ff14bdff4
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-02-11 15:19:53 +0300

    camp day 4 third question

---
+++ Camp/D4/bitwise_ors_of_subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarrayBitwiseORs(self, A: List[int]) -> int:
+        
+        discoveredSumms = {}
+        stack = []
+        for i in range(len(A)):
+            stack.append(A[i])
+            for j in range(len(stack) - 1, -1 , -1):
+                givenValue = stack.pop(j)
+                calculatedResult = givenValue | A[i]
+                if calculatedResult not in discoveredSumms:
+                    # newSet = set([i])
+                    # discoveredSumms[calculatedResult] = newSet
+                    discoveredSumms[calculatedResult] = set([i])
+                    stack.append(calculatedResult)
+                else:
+                    if i not in discoveredSumms[calculatedResult]:   ## not in set
+                        discoveredSumms[calculatedResult].add(i)
+                        stack.append(calculatedResult)
+        return len(discoveredSumms)
+                
