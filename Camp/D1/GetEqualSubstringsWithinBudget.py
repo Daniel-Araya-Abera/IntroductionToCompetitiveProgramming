c8e4e11d88fe45d8584bbbba6fedeaedcf63f076
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-02-11 15:06:38 +0300

    camp D1 2nd question

---
+++ Camp/D1/GetEqualSubstringsWithinBudget.py
@@ -0,0 +1,27 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        viableEnd = min(len(s), len(t))
+        start = 0
+        end = 1
+        count = i = 0
+        maxx = 0
+        limit = 0
+        for i in range(viableEnd):
+            end = i + 1
+            
+            first = ord(s[i]) - ord('a')
+            second = ord(t[i]) - ord('a')
+            diff = abs(first - second)
+            
+            limit = limit + diff
+
+            while limit > maxCost:        # first_diff = abs(   ( ord(s[start]) - ord('a') )   -    ( ord(t[start]) - ord('a') )   )
+                first_diff = abs( ord(s[start]) - ord(t[start])  )
+                limit = limit - first_diff
+                start += 1
+                
+            maxx = max(maxx, end - start)
+            # if end - start > maxx:
+            #     maxx = end - start
+        
+        return maxx
