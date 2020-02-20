e5ede8b95ab4cde7824793baaca367259d712c05
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-02-21 01:03:51 +0300

    Camp D11 A, ShiftingLetters

---
+++ Camp/D11/A, ShiftingLetters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shiftingLetters(self, S: str, shifts: List[int]) -> str:
+        frequency = [0] * len(shifts)
+        summ = sum(shifts)
+        frequency[0] = summ
+        for i in range(1, len(frequency)):
+            frequency[i] = frequency[i - 1] - shifts[i - 1]
+        
+        # print("frequency is ", frequency)
+        result = "" ## better use join function
+        for i in range(len(S)):
+            result += self.shiftSingleLetter( S[i], frequency[i] )
+        # print("result is ", result)
+        return result
+        
+        
+        
+    
+    def shiftSingleLetter(self, singleLetter: str, shift: int) -> str:
+        original = ord(singleLetter) - ord('a')
+        final = (original + shift) % 26 + ord('a')
+        final = chr(final)
+        return final
+        
