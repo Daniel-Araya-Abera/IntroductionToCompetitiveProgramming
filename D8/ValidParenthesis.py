c2e68589230573b7b7b4dba780c2303f88134b65
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-02-21 16:42:02 +0300

    D8 modified ValidParenthesis

--- D8/ValidParenthesis.py
+++ D8/ValidParenthesis.py
@@ -1,92 +1,23 @@
-# class Stack:
-#     def __init__(self):
-#         self.data = []
-    
-#     def push(self, givenNum):
-#         self.data.append(givenNum)
-#     def pop(self):
-#         self.data.pop()
-
-class Stack():
-    def __init__(self):
-        self.elements=[]
-    def push(self,item):
-        self.elements.append(item)
-        print("JUST added ", item)
-    def pop(self):
-        if len(self.elements)<=0:           
-            return None
-        else:
-            print("JUST removed ", self.elements[-1])
-            return self.elements.pop()
-    def top(self):
-        if len(self.elements)<=0:
-        
-            return None
-        else:
-            return self.elements[-1]
-    def size(self):
-        return len(self.elements)
-    
-    def isEmpty(self):
-        if(len(self.elements)==0):
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        if len(s) == 0:
             return True
-        return False
-
-##  () [] {}
-##  ( [ {{ () [] [(]) } () } ] )
-## [(])
-class Solution(object):
-    def isValid(self, s):
-        stack1 = Stack()
-##        for each in s:
-##            stack1.push(each)
-
-        if len(s) % 2:
-           return False
-        
-        for i in range((len(s) // 2)):
-            print("input is ", s)
+        stack.append(s[0])
+        for i in range(1, len(s)):
             if s[i] in "{[(":
-                stack1.push(s[i])
+                stack.append(s[i])
             else:
-                if stack1.isEmpty():
-                    break
-                value = stack1.pop()
-                if stack1.isEmpty():
-                    break
-                potentialPair = stack1.top()
-                if value == ")":
-                    print("a")
-                    if potentialPair == "(":
-                        stack1.pop()
-                    else:
-                        return False
-                elif value == "}":
-                    print("b")
-                    if potentialPair == "{":
-                        stack1.pop()
-                    else:
-                        return False
-                elif value == "]":
-                    print("c")
-                    if potentialPair == "[":
-                        stack1.pop()
-                    else:
-                        return False
+                if len(stack) == 0:
+                    return False
+                if s[i] == "}" and stack[-1] == "{":
+                    stack.pop()
+                elif s[i] == "]" and stack[-1] == "[":
+                    stack.pop()
+                elif s[i] == ")" and stack[-1] == "(":
+                    stack.pop()
                 else:
-                    print("e")
-                    return False   ## invalid chars on input
-                
-            if stack1.isEmpty():
-                break
-        if not(stack1.isEmpty()):
+                    return False
+        if len(stack):            
             return False
-
         return True
-            
-
-
-
-s = Solution()
-print( s.isValid("{[]}") )
