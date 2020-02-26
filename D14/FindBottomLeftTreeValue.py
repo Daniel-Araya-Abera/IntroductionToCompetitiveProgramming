eb7d2875ee45b67e7cd808502953daff820ac39c
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2020-02-26 14:12:14 +0300

    committed D14 Find BottomLeftTreeValue

---
+++ D14/FindBottomLeftTreeValue.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        queue = deque()
+        queue.append( (0, root) )
+        leftMostValues = { 0:root.val}
+        
+        while queue:
+            current = queue.popleft()
+            
+            if current:
+                leftSide = current[1].left
+                rightSide = current[1].right
+
+                if leftSide:
+                    queue.append(    (current[0] + 1, current[1].left )   ) 
+
+                    if (current[0] + 1) not in leftMostValues:
+                        leftMostValues[current[0] + 1] = current[1].left.val
+                    else:
+                        pass
+                        # print("index ", current[0] + 1, " is already in dictionary, value is ", leftMostValues[current[0] + 1])
+
+                if rightSide:
+                    queue.append(    (current[0] + 1, current[1].right )   ) 
+                    if (current[0] + 1) not in leftMostValues:
+                        leftMostValues[current[0] + 1] = current[1].right.val
+                    else:
+                        pass
+                        # print("index ", current[0] + 1, " is already in dictionary, value is ", leftMostValues[current[0] + 1])
+
+            
+        # print("left most value are : ", leftMostValues)
+        maxx = max(leftMostValues.keys())
+        # print("maxx is ", maxx)
+        return leftMostValues[maxx]
+    
+    
+    
+    
